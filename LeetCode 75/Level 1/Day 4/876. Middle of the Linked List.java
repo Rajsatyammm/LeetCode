635b8dac472300e8f9ffc883ca1d631fb7de0aa7
Satyam Raj <rajsatyammm@gmail.com>
2023-05-02 07:07:38 +0530

    Create 876. Middle of the Linked List.java

---
+++ LeetCode 75/Level 1/Day 4/876. Middle of the Linked List.java
@@ -0,0 +1,14 @@
+// 876. Middle of the Linked List
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
