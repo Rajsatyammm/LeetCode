473aff5d327c6049948f721a3665a9ecaf830b8b
Satyam Raj <rajsatyammm@gmail.com>
2023-05-02 07:06:28 +0530

    Create 142. Linked List Cycle II.java

---
+++ LeetCode 75/Level 1/Day 4/142. Linked List Cycle II.java
@@ -0,0 +1,26 @@
+// 142. Linked List Cycle II
+
+public class Solution {
+    
+    public ListNode detectCycle(ListNode head) {
+        // if(hasCycle(head)) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                slow = head;
+
+                while(slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
