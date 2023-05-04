7eb6338d345b0b270ad928f3b3f845bf4307aef2
Satyam Raj <rajsatyammm@gmail.com>
2023-05-04 19:43:35 +0530

    Create 242. Valid Anagram.java

---
+++ Data Structure/Data Structure I/Day 6 String/242. Valid Anagram.java
@@ -0,0 +1,32 @@
+// 242. Valid Anagram
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(int i=0; i<t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if(!map.containsKey(ch)) {
+                return false;
+            }
+            else {
+                int freq = map.get(ch);
+                if(freq == 1) {
+                    map.remove(ch);
+                }
+                else {
+                    map.put(ch, freq - 1);
+                }
+            }
+        }
+        if(!map.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+}
