3a7d9e98b1d5485131247c594540f292511b2aab
Satyam Raj <rajsatyammm@gmail.com>
2023-05-06 08:00:54 +0530

    Create 206. Reverse Linked List.java

---
+++ Data Structure/Data Structure I/Day 8 Linked list/206. Reverse Linked List.java
@@ -0,0 +1,23 @@
+// 206. Reverse Linked List
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode nextP = curr.next;
+            curr.next = prev;
+
+            //update
+            prev = curr;
+            curr = nextP;
+        }
+        head = prev;
+        return head;
+    }
+}
