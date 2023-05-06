0e09e233a572ef601660ebee1045567914730a5b
Satyam Raj <rajsatyammm@gmail.com>
2023-05-06 23:26:37 +0530

    Update 98. Validate Binary Search Tree.java

--- LeetCode 75/Level 1/Day 8 Binary Search Tree/98. Validate Binary Search Tree.java
+++ LeetCode 75/Level 1/Day 8 Binary Search Tree/98. Validate Binary Search Tree.java
@@ -23,3 +23,24 @@ class Solution {
         return true;
     }
 }
+
+
+// Approach 2 : 
+
+class Solution {
+    public boolean isValid(TreeNode root, TreeNode min, TreeNode max) {
+        if(root == null) {
+            return true;
+        }
+        if(min != null && min.val >= root.val) {
+            return false;
+        }
+        else if(max != null && max.val <= root.val) {
+            return false;
+        }
+        return isValid(root.left, min, root) && isValid(root.right, root, max);
+    }
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+}
