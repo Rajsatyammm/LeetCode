a7a0cc8745637074f9e716a68db173b4ef3d95c1
Satyam Raj <rajsatyammm@gmail.com>
2023-05-10 12:19:27 +0530

    Create 746. Min Cost Climbing Stairs.java

---
+++ LeetCode 75/Level 1/Day 11 Dynamic Programming/746. Min Cost Climbing Stairs.java
@@ -0,0 +1,18 @@
+// 746. Min Cost Climbing Stairs
+
+class Solution {
+    private int minCost(int[] cost, int[] dp, int n) {
+        if(n<=1) return 0;
+        if(n==2) return Math.min(cost[0], cost[1]);
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = Math.min(minCost(cost, dp, n-1) + cost[n-1], minCost(cost, dp, n-2) + cost[n-2]);
+        return dp[n];
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return minCost(cost, dp, n);
+    }
+}
