5303ebc32f4bcea9b7e6e85ef5d727d1b9bea227
Satyam Raj <rajsatyammm@gmail.com>
2023-05-13 08:16:01 +0530

    Update 15. 3Sum.java

--- Data Structure/Data Structure II/Day 1 Array/15. 3Sum.java
+++ Data Structure/Data Structure II/Day 1 Array/15. 3Sum.java
@@ -1,4 +1,36 @@
 // 15. 3Sum
+
+// Approach 1 : 
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length && nums[i] <= 0; ++i) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        }
+        return res;
+}
+    
+    void twoSumII(int[] nums, int i, List<List<Integer>> res) {
+        int lo = i + 1, hi = nums.length - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0 || (lo > i + 1 && nums[lo] == nums[lo - 1])) {
+                ++lo;
+            }
+            else if ( sum > 0 || (hi < nums.length - 1 && nums[hi] == nums[hi + 1])) {
+                --hi;
+            }
+            else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+            }
+        }
+    }
+}
+
+// Approach 2 :
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int target = 0;
