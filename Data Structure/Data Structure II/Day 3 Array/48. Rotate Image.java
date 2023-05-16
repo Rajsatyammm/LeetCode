b029d58171d3ed0aa0630bc5ea54cb26f2c2b06c
Satyam Raj <rajsatyammm@gmail.com>
2023-05-16 19:53:01 +0530

    Create 48. Rotate Image.java

---
+++ Data Structure/Data Structure II/Day 3 Array/48. Rotate Image.java
@@ -0,0 +1,25 @@
+// 48. Rotate Image
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        //transpose of matrix
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //reverse each row
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+    }
+}
