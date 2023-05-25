50b6681852d24d7c8ab19139c04237babc99dd98
Satyam Raj <rajsatyammm@gmail.com>
2023-05-25 15:30:23 +0530

    Create 837. New 21 Game.java

---
+++ 837. New 21 Game.java
@@ -0,0 +1,27 @@
+// 837. New 21 Game
+
+class Solution {
+    public double new21Game(int N, int K, int maxPts) {
+        // Corner cases
+        if (K == 0) return 1.0;
+        if (N >= K - 1 + maxPts) return 1.0;
+
+        // dp[i] is the probability of getting point i.
+        double[] dp = new double[N + 1];
+        Arrays.fill(dp, 0.0);
+
+        double probability = 0.0; // dp of N or less points.
+        double windowSum = 1.0; // Sliding required window sum
+        dp[0] = 1.0;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < K) windowSum += dp[i];
+            else probability += dp[i];
+
+            if (i >= maxPts) windowSum -= dp[i - maxPts];
+        }
+
+        return probability;
+    }
+}
