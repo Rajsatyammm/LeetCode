e3fcf2da72b65496fa0c5f90642ce131b41e58ac
Satyam Raj <rajsatyammm@gmail.com>
2023-05-27 10:09:39 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            
+            String S1 = read.readLine().trim();
+            String S2 = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int longestCommonSubstr(String S1, String S2, int n, int m){
+        // code here
+        int max = 0;
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i<n; i++ ) {
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<m; j++) {
+            dp[0][j] = 0;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(S1.charAt(i-1) == S2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    max = Math.max(max, dp[i][j]);
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return max;
+    }
+}
