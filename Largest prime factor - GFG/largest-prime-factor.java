e638f759ff930acc1f271fff95091c60a4666e15
Satyam Raj <rajsatyammm@gmail.com>
2023-08-09 21:35:51 +0530

    Added solution - LeetHub

---
+++ Largest prime factor - GFG/largest-prime-factor.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.largestPrimeFactor(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long largestPrimeFactor(int N) {
+        // code here
+        long n = 2;
+        // loop till sqrt(N)
+        while ((n * n) <=N)
+            if (N % n == 0) 
+                N /= n; 
+            else
+                n++; 
+        return N;
+    }
+}
