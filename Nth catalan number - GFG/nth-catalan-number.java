4c256cbe488c5f510a77fd3435afecf62a5beaf9
Satyam Raj <rajsatyammm@gmail.com>
2023-08-16 17:45:41 +0530

    Added solution - LeetHub

--- Nth catalan number - GFG/nth-catalan-number.java
+++ Nth catalan number - GFG/nth-catalan-number.java
@@ -1,48 +1,50 @@
 //{ Driver Code Starts
 import java.io.*;
 import java.util.*;
-import java.math.*;
+
 class GFG {
-	public static void main (String[] args) {
-	    
-	    //taking input using Scanner class
-		Scanner sc=new Scanner(System.in);
-		
-		//taking total count of testcases
-		int t=sc.nextInt();
-		while(t-->0){
-		    
-		    //taking number of elements
-		    int n=sc.nextInt();
-		   
-		    //calling findCatalan() method
-		    System.out.println(new Solution().findCatalan(n));
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.findCatalan(n);
+            
+            System.out.println(res);
+            
+        }
+    }
 }
+
 // } Driver Code Ends
 
 
 
-class Solution
-{
-    //Function to find the nth catalan number.
-    public static BigInteger findCatalan(int n)
-    {
-        //Your code here
-        BigInteger b = new BigInteger("1");
-        for (int i = 1; i <= n; i++) {
-            b = b.multiply(BigInteger.valueOf(i));
+
+
+
+class Solution {
+    public static int findCatalan(int n) {
+        // code here
+        int c[] = new int[n+1];
+        c[0] = c[1] = 1;
+        long mod = 1000_000_007L;
+        for(int i=2; i<=n; i++) {
+            long ans=0;
+            //c[i]=0;
+            for(int j=0; j<i; j++) {
+                ans=(ans + ((long) c[j] * c[i - j - 1]) % mod) % mod;
+            }
+            c[i] =(int) ans;
+           
         }
-        b = b.multiply(b);
- 
-        BigInteger d = new BigInteger("1");
-        for (int i = 1; i <= 2 * n; i++) {
-            d = d.multiply(BigInteger.valueOf(i));
-        }
-        BigInteger ans = d.divide(b);
- 
-        ans = ans.divide(BigInteger.valueOf(n + 1));
-        return ans;
+        return c[n];
     }
 }
+        
+        
