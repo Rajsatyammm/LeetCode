682aee9f6bb33769374795a6a401b6cc35bfce99
Satyam Raj <rajsatyammm@gmail.com>
2023-08-22 11:45:33 +0530

    Added solution - LeetHub

--- Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.java
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.java
@@ -65,6 +65,36 @@ class Solution {
         return f(price);
     }
     
+    static int f(int i, int maxTr, int[] price) {
+        if(i == price.length || maxTr == 0) return 0;
+        
+        if(maxTr % 2 == 0) { //buy case
+            return Math.max(-price[i] + f(i+1, maxTr-1, price), f(i+1, maxTr, price));
+        }
+        else { // sell case
+            return Math.max(price[i] + f(i+1, maxTr-1, price), f(i+1, maxTr, price));
+        }
+    }
+    
+    static int f(int[] price) {
+        int n = price.length;
+        int[][] dp = new int[n+1][5];
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        for(int j=0; j<5; j++) dp[0][j] = 0;
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int maxTr=1; maxTr<5; maxTr++) {
+                if(maxTr % 2 == 0) { //buy case
+                    dp[i][maxTr] = Math.max(-price[i] + dp[i+1][maxTr-1], dp[i+1][maxTr]);
+                }
+                else { // sell case
+                    dp[i][maxTr] = Math.max(price[i] + dp[i+1][maxTr-1], dp[i+1][maxTr]);
+                }
+            }
+        }
+        return dp[0][4];
+    }
+    
     // static int f(int i, int buy, int max, int[] price, int[][][] dp) {
     //     if(i == price.length) return 0;
     //     if(max == 0) return 0;
@@ -77,36 +107,38 @@ class Solution {
     //         return dp[i][buy][max] = Math.max(price[i] + f(i+1, 1, max-1, price, dp), f(i+1, 0, max, price, dp));
     // }
     
-    static int f(int[] price) {
-        int n = price.length;
-        int[][][] dp = new int[n+1][2][3];
-        for(int j=0; j<2; j++) {
-            for(int k=0; k<3; k++) {
-                dp[0][j][k] = 0;
-            }
-        }
+    // static int f(int[] price) {
+    //     int n = price.length;
+    //     int[][][] dp = new int[n+1][2][3];
+    //     for(int j=0; j<2; j++) {
+    //         for(int k=0; k<3; k++) {
+    //             dp[0][j][k] = 0;
+    //         }
+    //     }
         
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<2; j++) {
-                dp[i][j][0] = 0;
-            }
-        }
+    //     for(int i=0; i<=n; i++) {
+    //         for(int j=0; j<2; j++) {
+    //             dp[i][j][0] = 0;
+    //         }
+    //     }
         
         
-        for(int i=n-1; i>=0; i--) {
-            for(int buy=0; buy<=1; buy++) {
-                for(int max=1; max<=2; max++) {
-                    if(buy == 1) 
-                        dp[i][buy][max] = Math.max(-price[i] + dp[i+1][0][max], dp[i+1][1][max]);
-                    //sell
-                    else 
-                        dp[i][buy][max] = Math.max(price[i] + dp[i+1][1][max-1], dp[i+1][0][max]);
-                }
-            }
-        }
+    //     for(int i=n-1; i>=0; i--) {
+    //         for(int buy=0; buy<=1; buy++) {
+    //             for(int max=1; max<=2; max++) {
+    //                 if(buy == 1) 
+    //                     dp[i][buy][max] = Math.max(-price[i] + dp[i+1][0][max], dp[i+1][1][max]);
+    //                 //sell
+    //                 else 
+    //                     dp[i][buy][max] = Math.max(price[i] + dp[i+1][1][max-1], dp[i+1][0][max]);
+    //             }
+    //         }
+    //     }
         
-        return dp[0][1][2];
+    //     return dp[0][1][2];
         
-    }
+    // }
+    
+    
 }
         
